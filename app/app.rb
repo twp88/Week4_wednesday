cac643a3ced8ffb5d0534929dd23b7af9dfd959b
Rosie <r.gollancz@gmail.com>
2016-10-18 16:02:01 +0100

    Restructures directories

--- app/app.rb
+++ app/app.rb
@@ -1,14 +1,11 @@
 require 'sinatra/base'
+require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Hello BookmarkManager!'
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
-
-  #get '/links' do
-
-  #end
-
 
 
   # start the server if ruby file executed directly
